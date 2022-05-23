5bf24c53268f30120a96e686072a4a4847b1410d
samiulalimsaad <saadnbiuraj@gmail.com>
2022-05-23 23:03:40 +0600

    added user authentication

--- app.js
+++ app.js
@@ -9,8 +9,28 @@ const PORT = process.env.PORT || 5000;
 //  middleWares
 // enable cors
 app.use(cors());
-
 app.use(express.json());
+
+const verifyUser = async (req, res, next) => {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).json({
+            success: false,
+            message: "unauthorized access",
+        });
+    }
+    const token = authHeader.split(" ")[1];
+    await jwt.verify(token, process.env.ACCESS_TOKEN, (err, decoded) => {
+        if (err) {
+            return res.status(403).json({
+                success: false,
+                message: "Forbidden access",
+            });
+        }
+        req.email = decoded?.email;
+        next();
+    });
+};
 
 app.get("/", (req, res) => {
     res.json({ hello: "hello" });
@@ -28,3 +48,11 @@ app.listen(PORT, async () => {
         }
     );
 });
+
+/**
+ * let ids = ['id1','id2','id3']
+let data = await MyModel.find(
+  {'_id': { $in: ids}}
+);
+ * 
+ */
